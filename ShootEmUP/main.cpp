16194c09e9c26184df3d211f1b1343eabe84e2d1
TaykoWS <54807648+TaykoWS@users.noreply.github.com>
2021-04-17 16:15:18 +0200

    création du rectangle (player)

--- ShootEmUP/main.cpp
+++ ShootEmUP/main.cpp
@@ -1,8 +1,26 @@
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
+    //Créer sans le QT Game Engine à l'aide du tuto (trop de bug et de problème d'intégration)
     QApplication a(argc, argv);
+
+    // Créer la scene
+    QGraphicsScene* scene = new QGraphicsScene();
+
+    // Créer notre rectangle qui représente notre joueur
+    QGraphicsRectItem* rect = new QGraphicsRectItem();
+    rect->setRect(0,0,100,100); // On défini la position et la taille de notre rectangle
+
+    // Ajouter notre joueur dans la scene
+    scene->addItem(rect);
+
+    // Pour visualiser la scene nous devons rajouter notre "vue"
+    QGraphicsView* view = new QGraphicsView(scene);
+    view->show(); // Une "view" est invisible, la méthode show permet de la rendre visible
 
     return a.exec();
 }
